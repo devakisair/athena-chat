fec7059a4da464181207caebc869e88e7bc97c46
DevakiSai <k.ramanan.chennai@gmailcom>
2024-08-01 16:50:02 +0530

    labeltext

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { Form } from "@athena/forge";
+import { Root } from "@athena/forge";
+import "@athena/forge/dist/forge.css";
+import { Textarea } from "@athena/forge";
+import { FormField, FormFieldLayout } from "@athena/forge";
+
 export function App() {
-  return <h1>Chat</h1>;
+  return (
+    <Root>
+      <h1>Chat</h1>
+      <Form>
+        <FormField labelText="Message" id="message" inputAs={Textarea} />
+      </Form>
+    </Root>
+  );
 }
